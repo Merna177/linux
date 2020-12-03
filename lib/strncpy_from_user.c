5c5b690b410bad1233a770a53eb6c0af5c0eba58
merna177 <mernazakaria177@gmail.com>
2020-12-03 02:22:29 +0200

    lib, uaccess: record all the addresses ranges used in usercopy functions
    To detect any intersection happent between two fetches we have to record every address range used

--- lib/strncpy_from_user.c
+++ lib/strncpy_from_user.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/compiler.h>
+#include <linux/dfetch-detection.h>
 #include <linux/export.h>
 #include <linux/kasan-checks.h>
 #include <linux/thread_info.h>
@@ -120,6 +121,7 @@ long strncpy_from_user(char *dst, const char __user *src, long count)
 		if (user_read_access_begin(src, max)) {
 			retval = do_strncpy_from_user(dst, src, count, max);
 			user_read_access_end();
+			dfetch_add_address(src, retval, _RET_IP_, dst);
 			return retval;
 		}
 	}
